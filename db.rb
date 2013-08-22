233a79542497a287e5561ee70e377e0b23856315
Eric Stiens <estiens@server.fake>
2013-08-22 17:19:30 -0400

    a ton of refactoring

--- db.rb
+++ db.rb
@@ -31,88 +31,96 @@ class Database
 
   def select_a_contact
 	check_database
-  	if ! @contacts_array.empty?
-  	puts "Please enter a name, id number, or email address"
-  	attribute=gets.chomp
-  	@contacts_array.each do |contact|
-			if (contact.id.to_s == attribute) || (contact.firstname.downcase == attribute.downcase) || (contact.lastname.downcase == attribute.downcase) || (contact.email.downcase == attribute.downcase)
-				contact.display_single_contact
-			end
-		end
-	end
+  return if @contacts_array.empty?
+  puts "Please enter a name, id number, or email address"
+  attribute=gets.chomp
+  filtered = filtered_contacts(attribute)
+    if filtered.length == 0
+      puts "Sorry, I couldn't find any contact with the attribute: #{attribute}"
+      return false
+    end
+    puts "Here are the contacts that match #{attribute}"
+    filtered.each do |contact|
+      contact.display_single_contact
+    end
   end
 
 
   def delete_a_contact
   	check_database
-  	if ! @contacts_array.empty?
-  	puts "Please enter a name, id number, or email address"
-  	attribute=gets.chomp	
-  		@contacts_array.each do |contact|
-			
-			if (contact.id.to_s == attribute) || (contact.firstname.downcase == attribute.downcase) || (contact.lastname.downcase == attribute.downcase) || (contact.email.downcase == attribute.downcase)
-				puts "Are you sure you want to delete #{contact.firstname} #{contact.lastname}? Y/N"
-				print">>"
-					answer=gets.chomp
-					if answer=="Y"
-						@contacts_array.delete(contact)
-					elsif answer =="N"
-						puts "okay I won't delete it"
-						return false
-					else
-						puts "Please put Y or N"
-					end
-			else
-				puts "Sorry, I couldn't find any contact with the attribute: #{attribute}"
-				return false
-			end
+  	return if @contacts_array.empty?
+    puts "Please enter a name, id number, or email address"
+    attribute=gets.chomp
+    filtered = filtered_contacts(attribute)
+
+    if filtered.length == 0
+      puts "Sorry, I couldn't find any contact with the attribute: #{attribute}"
+      return false
+    end
+
+  	filtered.each do |contact|
+			puts "You are about to delete #{contact.firstname} #{contact.lastname}."
+			unless prompt_for_confirmation
+				puts "Ok, I won't do that"
+				next
+      end
+      @contacts_array.delete(contact)
 		end
 	end	
+  
+
+
+  def filtered_contacts(attribute)
+    @contacts_array.select do |c|
+      [:firstname, :lastname, :email].inject(c.id.to_s == attribute) do |previous, key|
+        previous || c.send(key).downcase == attribute.to_s.downcase
+      end
+    end
   end
 
+  def prompt_for_confirmation
+      puts "Is that what you want? Y/N"
+      print ">> "
+      confirmation = gets.chomp.downcase
+      return true if confirmation == "y"
+      return false if confirmation == "n"
+  end
 
 def modify_a_contact
   check_database
-  if ! @contacts_array.empty?
-  	puts "Please enter a name, id number, or email address"
-  	attribute=gets.chomp
-  	
-  	@contacts_array.each do |contact|
-		if (contact.id.to_s == attribute) || (contact.firstname.downcase == attribute.downcase) || (contact.lastname.downcase == attribute.downcase) || (contact.email.downcase == attribute.downcase)
-				puts "Modifying the following contact"
-				contact.display_single_contact
-				puts "Is that what you want? Y/N"
-				print ">> "
-				confirmation = gets.chomp.downcase
-					if confirmation == "y"
-						puts "What attribute would you like to modify? [F]irst Name, [L]ast Name, [E]mail, or [N]ote?"
-						attrchoice=gets.chomp.downcase
-							case attrchoice
-							when "f"
-								puts "What's the new first name?"
-								firstname=gets.chomp
-								contact.firstname=firstname
-							when "l"
-								puts "What's the new last name?"
-								lastname=gets.chomp
-								contact.lastname=lastname							
-							when "e"
-								puts "What's the new email?"
-								email=gets.chomp
-								contact.email=email
-							when "n"
-								puts "What's the new note?"
-								note=gets.chomp
-								contact.note=note
-							else "Sorry, that's not a valid choice."
-							end
-					elsif confirmation == "n"
-						puts "okay, returning to main menu"
-					else 
-						puts "please say Y or N"
-					end
-		end			
-		end
+  return if @contacts_array.empty?
+	puts "Please enter a name, id number, or email address"
+	attribute=gets.chomp
+
+	filtered_contacts(attribute).each do |contact|
+		puts "Modifying the following contact"
+		contact.display_single_contact
+    unless prompt_for_confirmation
+      puts "okay, returning to main menu"
+      next
+    end
+    puts "What attribute would you like to modify? [F]irst Name, [L]ast Name, [E]mail, or [N]ote?"
+    attrchoice=gets.chomp.downcase
+    case attrchoice
+    when "f"
+    	puts "What's the new first name?"
+    	firstname=gets.chomp
+    	contact.firstname=firstname
+    when "l"
+    	puts "What's the new last name?"
+    	lastname=gets.chomp
+    	contact.lastname=lastname							
+    when "e"
+    	puts "What's the new email?"
+    	email=gets.chomp
+    	contact.email=email
+    when "n"
+    	puts "What's the new note?"
+    	note=gets.chomp
+    	contact.note=note
+    else
+      "Sorry, that's not a valid choice."
+    end
   end
 end
 
